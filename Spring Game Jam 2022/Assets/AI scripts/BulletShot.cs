d4d8bb71ca712d328d92e14f497a6827aea28ed8
Ziad-107 <mohammadz7202@my.uwstout.edu>
2022-02-12 12:53:32 -0600

    Fixed the script
    Here's the final bullet move script

---
+++ Spring Game Jam 2022/Assets/AI scripts/BulletShot.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+   float copespeedX = 0f;
+    float copespeedY = 0f;
+    float angle = 0 * Mathf.Rad2Deg;
+  /*        Vector2 VectorFromAngle (float theta) {
+     return new Vector2 (Mathf.Cos(theta), Mathf.Sin(theta)); 
+     
+      
+  
+ }
+ transform.Translate (VectorFromAngle (theta)); */
+    
+    
+    // Start is called before the first frame update
+    void awake()
+    {
+
+        copespeedX = +1f;
+        copespeedY = +1f;
+        copespeedX = -1f;
+        copespeedY = -1f;
+
+        Vector2 MovDir = new Vector2(copespeedX, copespeedY).normalized;
+        Vector2 MovAng = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle));
+        //Vector2 MovAng = new Vector2(Mathf.Cos(theta),Mathf.Sin(theta))
+        float movespeed = 6f; 
+        //float theta = 15 * Mathf.Rad2Deg;
+        transform.Translate (MovDir *  movespeed * MovAng);
+    
+   
+    } 
+
+    //pull me
+
+    // Update is called once per frame
+    /*
+    void Update()
+    {
+        
+    } */
+
+    
+}
