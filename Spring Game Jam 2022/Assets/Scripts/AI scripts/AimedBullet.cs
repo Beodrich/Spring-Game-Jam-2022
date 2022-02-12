15ae70c93056148a35ea2d2dad6c3175b8d2c7a0
biggsMN <72932919+biggsMN@users.noreply.github.com>
2022-02-12 15:53:15 -0600

    bullets now destroy themselves
    bullets now destroy themselves

--- Spring Game Jam 2022/Assets/Scripts/AI scripts/AimedBullet.cs
+++ Spring Game Jam 2022/Assets/Scripts/AI scripts/AimedBullet.cs
@@ -4,45 +4,12 @@ using UnityEngine;
 
 public class AimedBullet : MonoBehaviour
 {
-    private Transform player;
-    private Vector2 target;
-    public float speed = 1f;
-    private float calculatedSpeed;
-    Vector3 dest;
-    // Start is called before the first frame update
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        target = new Vector2(player.position.x, player.position.y);
-        //target = target * 100;
-        dest = player.position - transform.position;
-        //target = new Vector2(dest.x, dest.y);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //calculatedSpeed = speed * Time.deltaTime;
-        //transform.position = Vector2.MoveTowards(transform.position, target, calculatedSpeed);
-        ////this.transform.position = Vector3.MoveTowards(transform.position, target, calculatedSpeed);
-
-        //if (transform.position.x == target.x && transform.position.y == target.y)
-        //{
-        //    Debug.Log("reached");
-        //    Destroy(gameObject);
-        //}
-        //else
-        //{
-        //    //Debug.l
-        //}
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Wall")
         {
             Destroy(gameObject);
-            // damage player
         }
     }
 
