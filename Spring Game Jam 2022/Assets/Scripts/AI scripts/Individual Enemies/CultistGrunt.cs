72b9de58fceea77c9e4e9c85fe033ecacab800a0
biggsMN <72932919+biggsMN@users.noreply.github.com>
2022-02-12 16:48:44 -0600

    misc changes

---
+++ Spring Game Jam 2022/Assets/Scripts/AI scripts/Individual Enemies/CultistGrunt.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CultistGrunt : EnemyAI
+{
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void AgressiveOrbit()
+    {
+        base.AgressiveOrbit();
+    }
+
+    protected override void ChargeAtPlayer()
+    {
+        base.ChargeAtPlayer();
+    }
+
+    protected override void DecideMove()
+    {
+        base.DecideMove();
+    }
+
+    protected override void Die()
+    {
+        base.Die();
+    }
+
+    protected override void FollowPlayer()
+    {
+        base.FollowPlayer();
+    }
+
+    protected override void move()
+    {
+        base.move();
+    }
+
+    protected override void Orbit()
+    {
+        base.Orbit();
+    }
+
+    protected override void shoot()
+    {
+        base.shoot();
+    }
+
+    protected override void TakeDamage()
+    {
+        base.TakeDamage();
+    }
+}
