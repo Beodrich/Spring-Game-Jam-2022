27b29329987afcd2e74c88c4f21d0f935be6aa9a
Ziad-107 <mohammadz7202@my.uwstout.edu>
2022-02-11 22:53:03 -0600

    Trig is rigged
    Modifiable bullet script

---
+++ Spring Game Jam 2022/Assets/AI scripts/BulletShot.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+ /*   float copespeedX = 0f;
+    float copespeedY = 0f;
+     Vector2 VectorFromAngle (float Angolos) {
+     return new Vector2 (Mathf.Cos(Angolos), Mathf.Sin(Angolos)); 
+ }
+    
+    // Start is called before the first frame update
+    void awake()
+    {
+        copespeedX = +1f;
+        copespeedY = +1f;
+        copespeedX = -1f;
+        copespeedY = -1f;
+
+        Vector2 MovDir = new Vector2(copespeedX, copespeedY).normalized;
+        float movespeed = 6f; 
+        float theta = 
+        transform.Translate (MovDir *  movespeed);
+        transform.translate (VectorFromAngle (Angolos));
+
+    } */
+
+    // Update is called once per frame
+    /*
+    void Update()
+    {
+        
+    } */
+
+    
+}
