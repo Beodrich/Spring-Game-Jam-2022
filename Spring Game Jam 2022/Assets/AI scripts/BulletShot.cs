64a99633ac0761b8526ae97ac18451b758f3b81e
Ziad-107 <mohammadz7202@my.uwstout.edu>
2022-02-12 09:13:20 -0600

    It is now functional
    Fixed the bullet direction script

--- Spring Game Jam 2022/Assets/AI scripts/BulletShot.cs
+++ Spring Game Jam 2022/Assets/AI scripts/BulletShot.cs
@@ -4,27 +4,36 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
- /*   float copespeedX = 0f;
+   float copespeedX = 0f;
     float copespeedY = 0f;
-     Vector2 VectorFromAngle (float Angolos) {
-     return new Vector2 (Mathf.Cos(Angolos), Mathf.Sin(Angolos)); 
+    float angle = 0 * Mathf.Rad2Deg;
+  /*        Vector2 VectorFromAngle (float theta) {
+     return new Vector2 (Mathf.Cos(theta), Mathf.Sin(theta)); 
+     
+      
+  
  }
+ transform.Translate (VectorFromAngle (theta)); */
+    
     
     // Start is called before the first frame update
     void awake()
     {
+
         copespeedX = +1f;
         copespeedY = +1f;
         copespeedX = -1f;
         copespeedY = -1f;
 
         Vector2 MovDir = new Vector2(copespeedX, copespeedY).normalized;
+        Vector2 MovAng = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle));
+        //Vector2 MovAng = new Vector2(Mathf.Cos(theta),Mathf.Sin(theta))
         float movespeed = 6f; 
-        float theta = 
-        transform.Translate (MovDir *  movespeed);
-        transform.translate (VectorFromAngle (Angolos));
-
-    } */
+        //float theta = 15 * Mathf.Rad2Deg;
+        transform.Translate (MovDir *  movespeed * MovAng);
+    
+   
+    } 
 
     // Update is called once per frame
     /*
